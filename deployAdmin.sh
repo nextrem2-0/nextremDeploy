294e22ef8b3ee3fcf629496382210eaa07f140ec
Alejandro Esteve Maza <alex.esteve98@gmail.com>
2020-02-13 09:15:42 +0100

    fix deploy admin

--- deployAdmin.sh
+++ deployAdmin.sh
@@ -1,19 +1,21 @@
 sudo chmod -R 777 *
 
-rm -rf nextremSinCompilar
-mkdir nextremSinCompilar
 sudo rm -rf ./admin
-cd nextremSinCompilar/
 git clone https://github.com/nextrem2-0/nextremDeploy.git
 
 sudo chmod -R 777 *
 
 
-cd ./nextremDeploy/
+SRCPATH="./nextremDeploy/*"
+DISTPATH="./admin/"
+rm -rf ${DISTPATH}
+mkdir ${DISTPATH}
+cp -r ${SRCPATH} ${DISTPATH}
+
 
 sudo docker container run -d -v /opt/nextrem/admin:/usr/local/apache2/htdocs  --name nextrem_admin --expose 80 -e VIRTUAL_HOST=admin.nextrem.pve2.fpmislata.com -e LETSENCRYPT_HOST=admin.nextrem.pve2.fpmislata.com --net nginx-net httpd:2.4.41
 
-cd  ../
+cd  ./nextremDeploy
 npm install
 npm run build
 
